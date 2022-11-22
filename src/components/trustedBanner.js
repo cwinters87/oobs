4f33ffea40204dffd2f8f2eff2b0ac556bb1f57b
Chris Winters <chris@tasksuite.com>
2022-11-22 16:07:45 -0600

    save point

--- src/components/trustedBanner.js
+++ src/components/trustedBanner.js
@@ -13,33 +13,22 @@ const TrustedBanner = () => {
         </div>      
         <h3 class={styles.header}>Trusted by Lenders<br />Worldwide</h3>
       </div>
-
-
-      
       <div class={styles.itemsContainer}>
         <div  class={styles.items}>
-          
-
           <div class={styles.item}>
             <div class={styles.count}><CountUp duration={1} delay={.5} end={500} enableScrollSpy/>%</div>
             <div class={styles.text}>Increase in Originations</div>
           </div>
-
           <div class={styles.item}>
             <div class={styles.count}><CountUp duration={1} delay={.5} end={30} enableScrollSpy />%</div>
             <div class={styles.text}>Reduction in Software Cost</div>
           </div>
-
           <div class={styles.item}>
             <div class={styles.count}><CountUp duration={1} delay={.5} end={5000} enableScrollSpy />+</div>
             <div class={styles.text}>Users</div>
           </div>
-
-
         </div>
       </div>
-      
-      
     </div>
   )
 }
