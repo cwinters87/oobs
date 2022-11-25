7e277aa043780d59c3a47c8cc05c16fb635f47b0
Chris Winters <chris@tasksuite.com>
2022-11-25 13:36:56 -0600

    loan card added

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,15 +6,15 @@ import MainGif from '../images/hero/hero-main.gif'
 
 const Hero = () => {
   return (
-    <div class={styles.container}>
-      <div class={styles.textContainer}>
-        <h1 class={styles.header}><span>Loan</span> Management, Origination, Servicing, and Credit <span>Software</span></h1>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.header}><span>Loan</span> Management, Origination, Servicing, and Credit <span>Software</span></h1>
         <p>TaskSuite combines 4 powerful systems into a single product, enabling lenders to streamline their operations.</p>
         <div className={styles.buttonContainer}>
           <Button className={styles.button}>Schedule a Demo</Button>
         </div>
       </div>
-      <div class={styles.imgContainer}>
+      <div className={styles.imgContainer}>
         <img src={MainGif} alt="product offering gif"/>
       </div>
   </div>
--- src/components/trustedBanner.js
+++ src/components/trustedBanner.js
@@ -5,26 +5,26 @@ import Logo from '../images/trustedBanner/tasksuite-icon.svg'
 
 const TrustedBanner = () => {
   return (
-    <div class={styles.container}>
+    <div className={styles.container}>
       <div className={styles.titleContainer}>
         <div>
           <img src={Logo} alt='tasksuite logo'/>
         </div>      
-        <h3 class={styles.header}>Trusted by Lenders<br />Worldwide</h3>
+        <h3 className={styles.header}>Trusted by Lenders<br />Worldwide</h3>
       </div>
-      <div class={styles.itemsContainer}>
-        <div  class={styles.items}>
-          <div class={styles.item}>
-            <div class={styles.count}><CountUp duration={1} delay={.5} end={500} enableScrollSpy/>%</div>
-            <div class={styles.text}>Increase in Originations</div>
+      <div className={styles.itemsContainer}>
+        <div  className={styles.items}>
+          <div className={styles.item}>
+            <div className={styles.count}><CountUp duration={1} delay={.5} end={500} enableScrollSpy/>%</div>
+            <div className={styles.text}>Increase in Originations</div>
           </div>
-          <div class={styles.item}>
-            <div class={styles.count}><CountUp duration={1} delay={.5} end={30} enableScrollSpy />%</div>
-            <div class={styles.text}>Reduction in Software Cost</div>
+          <div className={styles.item}>
+            <div className={styles.count}><CountUp duration={1} delay={.5} end={30} enableScrollSpy />%</div>
+            <div className={styles.text}>Reduction in Software Cost</div>
           </div>
-          <div class={styles.item}>
-            <div class={styles.count}><CountUp duration={1} delay={.5} end={5000} enableScrollSpy />+</div>
-            <div class={styles.text}>Users</div>
+          <div className={styles.item}>
+            <div className={styles.count}><CountUp duration={1} delay={.5} end={5000} enableScrollSpy />+</div>
+            <div className={styles.text}>Users</div>
           </div>
         </div>
       </div>
