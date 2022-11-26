633fcbd6f888d3ad8dbad11493d863ab8d160b55
Chris Winters <chris@tasksuite.com>
2022-11-25 18:26:27 -0600

    navbar fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import NavigationBar from "./navBar"
+import NavigationBar from "./navar.js"
 import Footer from "./footer"
 import Container from 'react-bootstrap/Container'
 import * as styles from './layout.module.css'
--- src/components/loanCard.js
+++ src/components/loanCard.js
@@ -29,5 +29,4 @@ const LoanCard = () => {
     )
 }
 
-
 export default LoanCard
