94ff1082ba0cc97ee1e216405b15a0581452c8ca
Chris Winters <chris@tasksuite.com>
2023-01-03 10:24:09 -0600

    removed job post

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import SpacingCard from '../components/spacingCard'
 // import { Link } from 'gatsby'
-import Pdf from '../../static/TaskSuiteOpenRole.pdf'
+
 
 const CareersPage = () => {
   return (
@@ -11,9 +11,10 @@ const CareersPage = () => {
         <SpacingCard margin={'55px'}/>
         {/* <Link to="/">-Back to Home</Link> */}
         <SpacingCard margin={'55px'}/>
-        <h4>Jobs Postings:</h4>
+        <h4>Jobs Postings: <br/><br/>Please check back later for open roles</h4>
         <ul>
-          <li><a href={Pdf} target='_blank' rel='noopener noreferrer'>Chief Executive Officer</a></li>
+          {/* <li><a href={Pdf} target='_blank' rel='noopener noreferrer'>Chief Executive Officer</a></li> */}
+          
           
         </ul>
         <div style={{height: "500px"}}></div>
