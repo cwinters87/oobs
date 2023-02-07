b51a67d74dc0ac0696293aac77a0c5d94761ace8
Chris Winters <chris@tasksuite.com>
2023-02-07 15:15:48 -0600

    tag update

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,18 +9,6 @@ module.exports = {
     siteUrl: `https://www.tasksuite.com`
   },
   plugins: [
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        defaults: {
-          quality: 100,
-          
-        }
-      }
-    },
-    `gatsby-transformer-sharp`,
-    // Plug in tracking ID for google analytics below...
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
@@ -35,11 +23,11 @@ module.exports = {
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
@@ -47,14 +35,27 @@ module.exports = {
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
           // Defaults to https://www.googletagmanager.com
-          origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // origin: "YOUR_SELF_HOSTED_ORIGIN",
           // Delays processing pageview events on route update (in milliseconds)
-          delayOnRouteUpdate: 0,
+          // delayOnRouteUpdate: 0,
         },
       },
     },
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          quality: 100,
+          
+        }
+      }
+    },
+    `gatsby-transformer-sharp`,
+    // Plug in tracking ID for google analytics below...
+    
     // {
     //   resolve: `gatsby-plugin-google-fonts`,
     //   options: {
