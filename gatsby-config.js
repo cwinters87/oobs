aff85a54fdb4184e1200fc6a709742d942f90b17
Chris Winters <chris@tasksuite.com>
2023-03-27 12:19:28 -0500

    gtag manager update

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,33 +12,29 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "GTM-WXTCT9W",
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        // gtagConfig: {
-        //   optimize_id: "OPT_CONTAINER_ID",
-        //   anonymize_ip: true,
-        //   cookie_expires: 0,
-        // },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: false,
-          // Avoids sending pageview hits from custom paths
-          // exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          // origin: "YOUR_SELF_HOSTED_ORIGIN",
-          // Delays processing pageview events on route update (in milliseconds)
-          // delayOnRouteUpdate: 0,
-        },
-      },
+      resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: "GTM-WXTCT9W",
+
+      // Include GTM in development.
+      //
+      // Defaults to false meaning GTM will only be loaded in production.
+      includeInDevelopment: false,
+
+      // datalayer to be set before GTM is loaded
+      // should be an object or a function that is executed in the browser
+      //
+      // Defaults to null
+      defaultDataLayer: { platform: "gatsby" },
+
+      
+      // Defaults to gatsby-route-change
+      // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+      // Defaults to false
+      enableWebVitalsTracking: true,
+      // Defaults to https://www.googletagmanager.com
+      // selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
+    },
     },
     `gatsby-plugin-sitemap`,
     'gatsby-plugin-robots-txt',
