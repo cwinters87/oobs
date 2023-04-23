7b19662c7f44ecd273492cfd3bc70aa5897d536a
Chris Winters <taylor314@gmail.com>
2023-04-23 16:42:30 -0500

    completeToolSetCard built

--- src/components/homePage/completeToolset.js
+++ src/components/homePage/completeToolset.js
@@ -17,37 +17,49 @@ const CompleteToolsetCard = () => {
                     <p>TaskSuite's all-in-one platform puts the control at your fingertips, whether you want <br/>to utilize the entire suite or just one of our award-winning solutions.</p>
                 </div>
             </div>
-
-            <div className={styles.subContainer} id={styles.cardContainer}>
-                <Link className={styles.card} to='/'>
-                    <h3 className={styles.cardTitle}>Loan<br/>Origination</h3>
-                    <p className={styles.cardSubTitle}>
-                        Online applications, documents, credit checks, and more.
-                    </p>
-                </Link>
-                <Link className={styles.card} to='/'>
-                    <h3 className={styles.cardTitle}>Loan<br/>Credit AI</h3>
-                    <p className={styles.cardSubTitle}>
-                        Seamless integration with credit bureaus for scoring and fraud protection.
-                    </p>
-                </Link>
-                <Link className={styles.card} to='/'>
-                    <h3 className={styles.cardTitle}>Loan<br/>Servicing</h3>
-                    <p className={styles.cardSubTitle}>
-                        Cloud-based loan support for reducing risk and centralinzaing your portfolio.
-                    </p>
-                </Link>
-                <Link className={styles.card} to='/'>
-                    <h3 className={styles.cardTitle}>Loan<br/>Management</h3>
-                    <p className={styles.cardSubTitle}>
-                        Automated loan application processing, tracking and reporting.
-                    </p>
-                </Link>
+            <div id={styles.cardContainer}>
+                <div className={styles.subContainer} id={styles.cardWrapper}>
+                    
+                    <Link className={styles.card} to='/'>
+                        <div className={styles.cardContent}>
+                            <h3 className={styles.cardTitle}>Loan<br/>Origination</h3>
+                            <p className={styles.cardSubTitle}>
+                                Online applications, documents, credit checks, and more.
+                            </p>
+                        </div>
+                    </Link>
+                    <Link className={styles.card} to='/'>
+                        <div className={styles.cardContent}>
+                            <h3 className={styles.cardTitle}>Loan<br/>Credit AI</h3>
+                            <p className={styles.cardSubTitle}>
+                                Seamless integration with credit bureaus for scoring and fraud protection.
+                            </p>
+                        </div>
+                    </Link>
+                    <Link className={styles.card} to='/'>
+                        <div className={styles.cardContent}>
+                            <h3 className={styles.cardTitle}>Loan<br/>Servicing</h3>
+                            <p className={styles.cardSubTitle}>
+                                Cloud-based loan support for reducing risk and centralinzaing your portfolio.
+                            </p>
+                        </div>
+                    </Link>
+                    <Link className={styles.card} to='/'>
+                        <div className={styles.cardContent}>
+                            <h3 className={styles.cardTitle}>Loan<br/>Management</h3>
+                            <p className={styles.cardSubTitle}>
+                                Automated loan application processing, tracking and reporting.
+                            </p>
+                        </div>
+                    </Link>
+                </div>
             </div>
 
             <div className={styles.subContainer} id={styles.imgContainer}>
-                <StaticImage id={styles.image} src="../../images/homePage/completeToolset.png" placeholder="" alt="tasksuite product" />
+                <div id={styles.imgWrapper}><StaticImage className={styles.containedImage} src="../../images/homePage/completeToolset.png" placeholder="" alt="tasksuite product" /></div>
             </div>
+           
+
         </div>
             
     )
