087e0b3b903cd96d30253d627ad582a0e0ba5d90
Chris Winters <taylor314@gmail.com>
2023-04-22 23:00:28 -0500

    error fixing

--- src/components/homePage/currentProcess.js
+++ src/components/homePage/currentProcess.js
@@ -17,55 +17,55 @@ const CurrentProcessCard = () => {
             <div className={styles.flexWrapper} id={styles.flexWrapperTop}>
                 <div className={(styles.cardsTopRow, styles.card)} id={styles.cardOne}>
                     <div className={styles.textContainer}>
-                        <p>WITH TASKSUITE</p>
-                        <h3>Simple</h3>
-                        <p>stuff</p>
+                        <p><span className={styles.cardHeader}>WITH TASKSUITE</span></p>
+                        <h3 className={styles.cardTitle}>Simple</h3>
+                        <p className={styles.cardSubTitle}>versus today's normal, which relies on numerous people using multiple tools, often leading to complex and fragile workflows.</p>
                     </div>
                     <div className={styles.buttonContainer}>
-                        <AnimatedArrow/>
+                        {/* <AnimatedArrow/> */}
                     </div>
                 </div>
                 <div className={(styles.cardsTopRow, styles.card)} id={styles.cardTwo}>
                 <div className={styles.textContainer}>
-                        <p>WITH TASKSUITE</p>
-                        <h3>Simple</h3>
-                        <p>stuff</p>
+                        <p><span className={styles.cardHeader}>WITH TASKSUITE</span></p>
+                        <h3 className={styles.cardTitle}>Fast</h3>
+                        <p className={styles.cardSubTitle}>versus today, which requires corroborating data across multiple systems and reconciling differences.</p>
                     </div>
                     <div className={styles.buttonContainer}>
-                        <AnimatedArrow/>
+                        {/* <AnimatedArrow/> */}
                     </div>
                 </div>
             </div>
 
             <div className={styles.flexWrapper} id={styles.flexWrapperBottom}>
                 <div className={(styles.cardsBottomRow, styles.card)} id={styles.cardThree}>
-                <div className={styles.textContainer}>
-                        <p>WITH TASKSUITE</p>
-                        <h3>Simple</h3>
-                        <p>stuff</p>
+                    <div className={styles.textContainer}>
+                        <p><span className={styles.cardHeader}>WITH TASKSUITE</span></p>
+                        <h3 className={styles.cardTitle}>Trackable</h3>
+                        <p className={styles.cardSubTitle}>versus a current opaque proces with low visibility into statuses and dispersed activities.</p>
                     </div>
                     <div className={styles.buttonContainer}>
-                        <AnimatedArrow/>
+                        {/* <AnimatedArrow/> */}
                     </div>
                 </div>
                 <div className={(styles.cardsBottomRow, styles.card)} id={styles.cardFour}>
-                <div className={styles.textContainer}>
-                        <p>WITH TASKSUITE</p>
-                        <h3>Simple</h3>
-                        <p>stuff</p>
+                    <div className={styles.textContainer}>
+                        <p><span className={styles.cardHeader}>WITH TASKSUITE</span></p>
+                        <h3 className={styles.cardTitle}>Efficient</h3>
+                        <p className={styles.cardSubTitle}>versus an existing laborious loan sourcing, servicing, and management that ultimately costs time and money losses.</p>
                     </div>
                     <div className={styles.buttonContainer}>
-                        <AnimatedArrow/>
+                        {/* <AnimatedArrow/> */}
                     </div>
                 </div>
                 <div className={(styles.cardsBottomRow, styles.card)} id={styles.cardFive}>
-                <div className={styles.textContainer}>
-                        <p>WITH TASKSUITE</p>
-                        <h3>Simple</h3>
-                        <p>stuff</p>
+                    <div className={styles.textContainer}>
+                        <p><span className={styles.cardHeader}>WITH TASKSUITE</span></p>
+                        <h3 className={styles.cardTitle}>Customizable</h3>
+                        <p className={styles.cardSubTitle}>versus today's process of utilizing generic software that is rigid and not designed around the needs of the loan industry.</p>
                     </div>
                     <div className={styles.buttonContainer}>
-                        <AnimatedArrow/>
+                        {/* <AnimatedArrow/> */}
                     </div>
                 </div>
             </div>
