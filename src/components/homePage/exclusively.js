bc9f9ddda86aa5f4338fa9944f7e271e2dbfb5dc
Chris Winters <taylor314@gmail.com>
2023-05-01 15:18:39 -0500

    save point

--- src/components/homePage/exclusively.js
+++ src/components/homePage/exclusively.js
@@ -2,6 +2,8 @@ import * as React from 'react'
 import { StaticImage } from "gatsby-plugin-image"
 import ArrowLink from '../buttons/arrowLink'
 import * as styles from './exclusively.module.css'
+import Logo from '../../images/homePage/logo-robot.png'
+import Image from '../../images/homePage/testimonial.png'
 
 
 const ExclusivelyCard = () => {
@@ -10,13 +12,22 @@ const ExclusivelyCard = () => {
     <div className={styles.sectionWrapper}>
         <div className={styles.container}>
           <div className={styles.subContainer}>
-            <div className={styles.textContainer}>
-                <div id={styles.logoContainer}><StaticImage id={styles.logo} src="../../images/homePage/logo-robot.png" placeholder="" alt="TaskSuite robot" /></div>
-                <div id={styles.titleContainer}><h2 id={styles.title}>Created exclusively for the unique needs of the loan industry.</h2></div>
-                <div id={styles.subTitleContainer}><p id={styles.subTitle}>Created by loan veterans, TaskSuite was built from the ground up to deliver a complete solution for the complex needs of brokers and lenders. We understand the complexities and nuances of the loan industry, which is why we've been able to ensure our solution is tailored to meet your needs and use cases.</p></div>
-                <div id={styles.linkContainer}><ArrowLink text={'About the company'} route={'/'}/></div>
+            <div className={styles.square}>
+              <div className={styles.content}>
+                <div id={styles.textContainer}>
+                    <div id={styles.logoContainer}><img id={styles.logo} src={Logo} alt='tasksuite robot'/></div>
+                    <div id={styles.titleContainer}><h2 id={styles.title}>Created exclusively for the unique needs of the loan industry.</h2></div>
+                    <div id={styles.subTitleContainer}><p id={styles.subTitle}>Created by loan veterans, TaskSuite was built from the ground up to deliver a complete solution for the complex needs of brokers and lenders. We understand the complexities and nuances of the loan industry, which is why we've been able to ensure our solution is tailored to meet your needs and use cases.</p></div>
+                    <div id={styles.linkContainer}><ArrowLink text={'About the company'} route={'/'}/></div>
+                </div>
+              </div>
             </div>
-            <div className={styles.imgContainer}><StaticImage id={styles.image} src='../../images/homePage/testimonial.png'/></div>
+            
+            <div className={styles.square}>
+              <div className={styles.content}>
+                <div id={styles.imgContainer}><img src={Image} alt='laptop on desk'/></div>
+              </div>
+            </div>
           </div>
         </div>
         </div>
