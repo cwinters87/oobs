560b71d04b0942d22391ede49cc75fd93ad5226f
Chris Winters <taylor314@gmail.com>
2023-05-03 16:35:19 -0500

    save point

--- src/components/homePage/currentProcess.js
+++ src/components/homePage/currentProcess.js
@@ -16,6 +16,21 @@ const CurrentProcessCard = () => {
             <p id={styles.subTitle}>We know you aren't becasue we've been there too. TaskSuite was built for and by loan management veterans to be the platform that simply didn't exist.</p>
         </div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        {/* destop view render */}
         <div className={styles.subContainer} id={styles.cardContainer}>
 
 
@@ -87,6 +102,128 @@ const CurrentProcessCard = () => {
             
 
         </div>
+
+
+
+
+
+        {/* mobile view render */}
+        <div className={styles.subContainer} id={styles.cardContainerMobile}>
+
+
+            <div className={styles.firstRow}>
+                <div className={styles.cardMobile} id={styles.cardOneMobile}>
+                        
+                        <div className={styles.cardOneWrapper}>
+                            <div className={styles.textContainer}>
+                                <p><span className={styles.cardHeader}>WITH TASKSUITE</span></p>
+                                <h3 className={styles.cardTitle}>Simple</h3>
+                                <p className={styles.cardSubTitle}>versus today's normal, which relies on numerous people using multiple tools, often leading to complex and fragile workflows.</p>
+                                
+                            </div>
+                        </div>
+                        <div className={styles.cardOneWrapper}>
+                            <div className={styles.imgWrapperMobile}>
+                                        <div className={styles.imgContainerMobile}>
+                                            <picture>
+                                                <source media="(max-width: 991px)" srcSet={ImageMobile} />
+                                                <source media="(min-width: 992px)" srcSet={Image} />
+                                                <img src={Image} alt="TaskSuite product" style={{width: "auto", height: "100%"}} />
+                                            </picture>
+                                        </div>
+                                </div>
+                        </div>
+                        
+                        <div className={styles.buttonContainer}>
+                            {/* <AnimatedArrow/> */}
+                        </div>
+                    </div>
+            </div>
+
+
+
+
+            <div className={styles.secondRow}>
+                <div className={styles.cardMobile} id={styles.cardTwoMobile}>
+                        <div className={styles.textContainer}>
+                            <p><span className={styles.cardHeader}>WITH TASKSUITE</span></p>
+                            <h3 className={styles.cardTitle}>Fast</h3>
+                            <p className={styles.cardSubTitle}>versus today, which requires corroborating data across multiple systems and reconciling differences.</p>
+                        </div>
+                        <div className={styles.buttonContainer}>
+                            {/* <AnimatedArrow/> */}
+                        </div>
+                    </div>
+                
+
+                
+                    <div className={styles.cardMobile} id={styles.cardThreeMobile}>
+                        <div className={styles.textContainer}>
+                            <p><span className={styles.cardHeader}>WITH TASKSUITE</span></p>
+                            <h3 className={styles.cardTitle}>Trackable</h3>
+                            <p className={styles.cardSubTitle}>versus a current opaque proces with low visibility into statuses and dispersed activities.</p>
+                        </div>
+                        <div className={styles.buttonContainer}>
+                            {/* <AnimatedArrow/> */}
+                        </div>
+                    </div>
+            </div>
+
+
+
+
+
+            <div className={styles.thirdRow}>
+                <div className={styles.cardMobile} id={styles.cardFourMobile}>
+                        <div className={styles.textContainer}>
+                            <p><span className={styles.cardHeader}>WITH TASKSUITE</span></p>
+                            <h3 className={styles.cardTitle}>Efficient</h3>
+                            <p className={styles.cardSubTitle}>versus an existing laborious loan sourcing, servicing, and management that ultimately costs time and money losses.</p>
+                        </div>
+                        <div className={styles.buttonContainer}>
+                            {/* <AnimatedArrow/> */}
+                        </div>
+                    </div>
+
+
+
+
+                    <div className={styles.cardMobile} id={styles.cardFiveMobile}>
+                        <div className={styles.textContainer}>
+                            <p><span className={styles.cardHeader}>WITH TASKSUITE</span></p>
+                            <h3 className={styles.cardTitle}>Customizable</h3>
+                            <p className={styles.cardSubTitle}>versus today's process of utilizing generic software that is rigid and not designed around the needs of the loan industry.</p>
+                        </div>
+                        <div className={styles.buttonContainer}>
+                            {/* <AnimatedArrow/> */}
+                        </div>
+                    </div>
+            </div>
+
+            
+            
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <div className={styles.subContainer} id={styles.linkContainer}>
             <ArrowLink text={'For Lenders'} route={'/'}/>
             <ArrowLink text={'For Brokers'} route={'/'}/>
