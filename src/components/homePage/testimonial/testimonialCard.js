e46e94b7150d4f28a6bf83fe20886ee78877ae78
Chris Winters <taylor314@gmail.com>
2023-05-04 18:43:13 -0500

    spelling fix

--- src/components/homePage/testimonial/testimonialCard.js
+++ src/components/homePage/testimonial/testimonialCard.js
@@ -8,13 +8,13 @@ const cardData = [
   {
     imageUrl: backgroundImg,
     quote: 'We’ve worked with TaskSuite to develop a customized product that is tailored to our exact business needs. They’ve done this seamlessly',
-    author: 'Tuker S.',
+    author: 'Tucker S.',
     position: 'COO',
     company: 'BACKD',
   },
   {
     imageUrl: backgroundImg,
-    quote: 'TaskSuite has provide ongoing support and valuable insight as to how we can continue to utilize these tools to increase productivity and operate our business efficiently.',
+    quote: 'TaskSuite has provided ongoing support and valuable insight as to how we can continue to utilize these tools to increase productivity and operate our business efficiently.',
     author: 'Brandon Hoye',
     position: 'CTO',
     company: 'TUA FINANCIAL',
