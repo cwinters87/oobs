f23380bff2fb66159234937971328a0b49590d99
Chris Winters <chris@tasksuite.com>
2023-05-08 11:03:14 -0500

    terms page

--- src/components/buttons/scrollButton.js
+++ src/components/buttons/scrollButton.js
@@ -1,47 +1,47 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button'
 import * as styles from './scrollButton.module.css'
 
 const ScrollButton = ({propScrollButtonStyle, text}) => {
     
-    const handleClick = () => {
-        const scrollStep = window.innerHeight / 20; // adjust scroll step as desired
-        const startElement = document.getElementById("start");
-        const startPosition = startElement ? startElement.offsetTop : window.pageYOffset;
-        const stopElement = document.getElementById("stop");
-        const stopPosition = stopElement ? stopElement.offsetTop : document.body.offsetHeight;
-        const scrollInterval = setInterval(() => {
-          if (window.pageYOffset + window.innerHeight >= document.body.offsetHeight) {
-            clearInterval(scrollInterval);
-            removeEventListeners();
-          } else if (stopElement && window.pageYOffset + window.innerHeight >= stopPosition) {
-            clearInterval(scrollInterval);
-            removeEventListeners();
-            window.scrollTo(0, stopPosition);
-          } else {
-            window.scroll(0, window.pageYOffset + scrollStep);
-          }
-        }, 25); // adjust interval time as desired
+    // const handleClick = () => {
+    //     const scrollStep = window.innerHeight / 20 // adjust scroll step as desired
+    //     const startElement = document.getElementById("start")
+    //     const startPosition = startElement ? startElement.offsetTop : window.pageYOffset
+    //     const stopElement = document.getElementById("stop")
+    //     const stopPosition = stopElement ? stopElement.offsetTop : document.body.offsetHeight
+    //     const scrollInterval = setInterval(() => {
+    //       if (window.pageYOffset + window.innerHeight >= document.body.offsetHeight) {
+    //         clearInterval(scrollInterval)
+    //         removeEventListeners()
+    //       } else if (stopElement && window.pageYOffset + window.innerHeight >= stopPosition) {
+    //         clearInterval(scrollInterval)
+    //         removeEventListeners()
+    //         window.scrollTo(0, stopPosition)
+    //       } else {
+    //         window.scroll(0, window.pageYOffset + scrollStep)
+    //       }
+    //     }, 25); // adjust interval time as desired
     
-        const removeEventListeners = () => {
-          window.removeEventListener("wheel", stopScroll);
-          window.removeEventListener("keydown", stopScroll);
-          window.removeEventListener("mousedown", stopScroll);
-        };
+    //     const removeEventListeners = () => {
+    //       window.removeEventListener("wheel", stopScroll)
+    //       window.removeEventListener("keydown", stopScroll)
+    //       window.removeEventListener("mousedown", stopScroll)
+    //     };
     
-        const stopScroll = () => {
-          clearInterval(scrollInterval);
-          removeEventListeners();
-        };
+    //     const stopScroll = () => {
+    //       clearInterval(scrollInterval)
+    //       removeEventListeners()
+    //     };
     
-        window.addEventListener("wheel", stopScroll);
-        window.addEventListener("keydown", stopScroll);
-        window.addEventListener("mousedown", stopScroll);
+    //     window.addEventListener("wheel", stopScroll)
+    //     window.addEventListener("keydown", stopScroll)
+    //     window.addEventListener("mousedown", stopScroll)
     
-        if (startElement) {
-          window.scrollTo(0, startPosition);
-        }
-      };
+    //     if (startElement) {
+    //       window.scrollTo(0, startPosition)
+    //     }
+    //   };
     
     let buttonText = 'See how it works'
 
@@ -49,8 +49,15 @@ const ScrollButton = ({propScrollButtonStyle, text}) => {
         buttonText = text
       }
 
+    const scroll = () => {
+      const section = document.querySelector( '#start' )
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      
+    }
+
+
     return (
-        <Button className={styles.button} style={propScrollButtonStyle} onClick={handleClick}>
+        <Button className={styles.button} style={propScrollButtonStyle} onClick={scroll}>
             <span>{buttonText}</span>
         </Button>
     )
