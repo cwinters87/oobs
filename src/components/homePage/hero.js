df11b6f4d01f7b063b30e9b21364268585062ea1
Chris Winters <chris@tasksuite.com>
2023-05-12 11:49:50 -0500

    features mobile down

--- src/components/homePage/hero.js
+++ src/components/homePage/hero.js
@@ -32,7 +32,7 @@ const Hero = () => {
               {/* pass in: text={'text of your choice'} to change the text of the button
                   pass in objects as style to customize style */}
               <DemoButton  propButtonStyle={buttonStyle} text={'Get started'}/>
-              <ScrollButton propScrollButtonStyle={scrollButtonStyle} />
+              <ScrollButton propScrollButtonStyle={scrollButtonStyle} route={'/loansuite'}/>
             </div>
           </section>
           <section className={styles.subContainer} id={styles.subContainerTwo}>
