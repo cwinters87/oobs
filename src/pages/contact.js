aa911307d20dc4291e1a1eb60e27f4e660d9213d
Chris Winters <taylor314@gmail.com>
2023-06-09 14:10:35 -0500

    mobile first pass done

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,56 +1,10 @@
 import * as React from 'react'
 import { Seo } from "../components/seo"
 import Layout from "../layout/layout"
-// import Calendar from '../components/calendar/calendar'
-import ComingSoonCard from '../components/comingSoon'
 import DemoFormFullPageCard from '../components/demoForm/demoFormFullPage'
 
 
 const ContactPage = () => {
-
-  const mainStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    padding: '14% 7% 7% 7%',
-    gap: '20px'
-  }
-
-  const titleStyle = {
-    color: 'red',
-    fontSize: '50px',
-    fontFamily: 'var(--font-regular)',
-    fontWeight: '600',
-    color: 'var(--money-green)',
-    textAlign: 'center'
-  }
-
-  const subTitleStyle = {
-    color: 'blue',
-    fontSize: '30px',
-    fontFamily: 'var(--font-light)',
-    color: 'var(--primary-font-color)',
-    textAlign: 'center'
-  }
-
-  const calendarWrapperStyle = {
-    width: '100%',
-    height: '50vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-
-  const calendarStyle = {
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    border: '1px solid black'
-  }
 
   return (
     <Layout>
