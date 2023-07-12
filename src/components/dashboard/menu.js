342cd2bf3cf1f0f0e1f75a131144eda4a76958a1
Chris Winters <taylor314@gmail.com>
2023-07-11 22:01:18 -0500

    dashboard styling

--- src/components/dashboard/menu.js
+++ src/components/dashboard/menu.js
@@ -7,17 +7,14 @@ const DashboardMenu = () => {
   return (
         <div className={styles.sectionWrapper}>
             <div className={styles.container}>
-                <h2 className={styles.title}>Dashboard</h2>
+                <h2 className={styles.title}>Docs</h2>
                 <ul className={styles.list}>
                     <li className={styles.listItem}>
                         <Link className={styles.link} to='/development/docs'>TaskSuite API Reference</Link>
                     </li>
                 </ul>
-
-                
             </div>
         </div>
-            
     )
 }
 
