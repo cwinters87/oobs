9ace4635b2a63e7b86219163383a062be10c3f1b
Lukas van der Merwe <lukas@taskflow.co.za>
2023-07-17 15:06:56 +0200

    [REF] MCD-647: Refactored the navbar component to use MUI

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,14 +1,14 @@
 import React from "react"
-import NavigationBar from "./navbar"
+import NavBar from "./navbar"
 import Footer from "./footer"
-import * as styles from './layout.module.css'
+import * as styles from "./layout.module.css"
 
 export default function Layout({ children }) {
   return (
     <div className={styles.siteWrapper}>
-        <NavigationBar/>
-          {children}    
-        <Footer/>
+      <NavBar />
+      {children}
+      <Footer />
     </div>
   )
 }
