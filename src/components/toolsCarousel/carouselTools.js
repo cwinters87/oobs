25850b8b1ad5e51e02a779dfce56199ed00dc6f6
Chris Winters <chris@tasksuite.com>
2023-10-03 15:14:11 -0500

    tool company updates

--- src/components/toolsCarousel/carouselTools.js
+++ src/components/toolsCarousel/carouselTools.js
@@ -1,26 +1,30 @@
 import React, { useEffect } from "react";
 
-import SlackImg from "../../images/toolCarousel/slack.png"
-import CreditsafeImg from "../../images/toolCarousel/creditsafe.png"
+import HeronImg from "../../images/toolCarousel/heron.png"
+import TwilioImg from "../../images/toolCarousel/twilio.svg"
+import FlinksImg from "../../images/toolCarousel/flinks.svg"
 import ExperianImg from "../../images/toolCarousel/experian.png"
 import OcrolusImg from "../../images/toolCarousel/ocrolus.png"
 import UsioImg from "../../images/toolCarousel/usio.png"
 import PlaidImg from "../../images/toolCarousel/plaid.png"
 import TransUnionImg from "../../images/toolCarousel/transunion.png"
 import EquifaxImg from "../../images/toolCarousel/equifax.png"
+import ProveImg from "../../images/toolCarousel/prove.png"
 
 import "./carouselTools.css"
 
 const CarouselTools = () => {
   const data = [
-    { image: SlackImg, title: "Slack" },
-    { image: CreditsafeImg, title: "Creditsafe" },
+    { image: HeronImg, title: "Heron Data" },
+    { image: TwilioImg, title: "Twilio" },
+    { image: FlinksImg, title: "Flinks" },
     { image: ExperianImg, title: "Experian" },
     { image: UsioImg, title: "Usio" },
     { image: OcrolusImg, title: "Ocrolus" },
     { image: PlaidImg, title: "Plaid" },
     { image: TransUnionImg, title: "TransUnion" },
     { image: EquifaxImg, title: "Equifax" },
+    { image: ProveImg, title: "Prove" },
   ]
 
   useEffect(() => {
